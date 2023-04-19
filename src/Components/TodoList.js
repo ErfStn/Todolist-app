0f809183e20f5938606ad5427c1bbdbb71fe744f
Erfan Soltani <erfstn@gmail.com>
2023-04-19 07:56:08 +0430

    change style

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -3,36 +3,32 @@ import Todo from "./todo";
 import TodoForm from "./TodoForm";
 
 const TodoList = ({ todos, onCompleted, onDelete, onUpdateTodo }) => {
-    const [edit, setEdit] = useState({ id: null, text: "", isCompleted: false });
-    
-  const editTodo = (newInputValue) => {
-      onUpdateTodo(edit.id, newInputValue);
-      setEdit({id: null, text: "", isCompleted: false })
-  };
-  const renderTodos = () => {
-    if (todos.length === 0) return <p>add some todos</p>;
+	const [edit, setEdit] = useState({ id: null, text: "", isCompleted: false });
 
-    return todos.map((todo) => {
-      return (
-        <Todo
-          todo={todo}
-          key={todo.id}
-          onCompleted={() => onCompleted(todo.id)}
-          onDelete={() => onDelete(todo.id)}
-          onEdit={() => setEdit(todo)}
-        />
-      );
-    });
-  };
-  return (
-    <div>
-      {edit.id ? (
-        <TodoForm submitTodo={editTodo} edit={edit} />
-      ) : (
-        renderTodos()
-      )}
-    </div>
-  );
+	const editTodo = newInputValue => {
+		onUpdateTodo(edit.id, newInputValue);
+		setEdit({ id: null, text: "", isCompleted: false });
+	};
+	const renderTodos = () => {
+		if (todos.length === 0) return <p>add some todos</p>;
+
+		return todos.map(todo => {
+			return (
+				<Todo
+					todo={todo}
+					key={todo.id}
+					onCompleted={() => onCompleted(todo.id)}
+					onDelete={() => onDelete(todo.id)}
+					onEdit={() => setEdit(todo)}
+				/>
+			);
+		});
+	};
+	return (
+		<>
+			{edit.id ? <TodoForm submitTodo={editTodo} edit={edit} /> : renderTodos()}
+		</>
+	);
 };
 
 export default TodoList;
--- src/Components/todo.js
+++ src/Components/todo.js
@@ -1,13 +1,23 @@
-const Todo = ({todo ,onCompleted,onDelete,onEdit}) => {
-    return ( 
-        <div className="todo">
-            <div onClick={onCompleted} className={`textTodo ${todo.isCompleted ? "completed" : ""}`}>{todo.text}</div>
-            <div>
-                <button onClick={onEdit} className="btn" >Edit</button>
-                <button onClick={onDelete} className="btn remove" >Delete</button>
-            </div>
-        </div>
-     );
-}
- 
+const Todo = ({ todo, onCompleted, onDelete, onEdit }) => {
+	return (
+		<div className="todo">
+			<div
+				onClick={onCompleted}
+				className={`textTodo ${todo.isCompleted ? "completed" : ""}`}
+			>
+				<input type="checkbox" checked={todo.isCompleted} />
+				<span>{todo.text}</span>
+			</div>
+			<div>
+				<button onClick={onEdit} className="btn">
+					Edit
+				</button>
+				<button onClick={onDelete} className="btn remove">
+					Delete
+				</button>
+			</div>
+		</div>
+	);
+};
+
 export default Todo;
